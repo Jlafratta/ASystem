5377bd046bad9c7e6b725dc93a0d4f74c9ddef27
NicoGarcia137 <43713775+NicoGarcia137@users.noreply.github.com>
2020-03-29 20:16:58 -0300

    v2.1

--- Views/profesional_meeting.php
+++ Views/profesional_meeting.php
@@ -9,15 +9,69 @@
 
     <body class="bgdark">
 
-            <div class="container rounded my-3  ">
+
+
+
+
+
+        <div class="container rounded my-3">
+           
+        
+                <div class="container rounded p-3 my-3 bg-white">
+               
+                    
+                    <span class="titlespan"><?php echo(date("l d/m",time())); ?> </span>
+                    
+                    <hr>
+                    <div class="container inner rounded ">
+                        <br>
+                    <table class="table table-striped table-hover bg-white">
+                        <thead class="thead-dark">
+                            <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Paciente</th>
+                            <th scope="col">Hora</th>
+                            <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <th scope="row">1</th>
+                            <td>Bazan, Martin</td>
+                            <td>12:00</td>
+                            <td><button type="submit" class="btn btn-sm btn-outline-secondary float-right">Ingresar</button></td>
+                            </tr>
+                            <tr>
+                            <th scope="row">2</th>
+                            <td>Costas, Sebastian</td>
+                            <td>16:00</td>
+                            <td><button type="submit" class="btn btn-sm btn-outline-secondary float-right">Ingresar</button></td>
+                            </tr>
+                            <tr>
+                            <th scope="row">3</th>
+                            <td>Alvarez, Jose</td>
+                            <td>18:00</td>
+                            <td><button type="submit" class="btn btn-sm btn-outline-secondary float-right">Ingresar</button></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    </div>
+                </div>
+            
+        </div>
+
+
+
+
+            <div class="container rounded my-3 ">
 
             <aside>
                 <div class="container rounded p-3 my-3 bg-white">
-                    <span class="titlespan">Pacientes</span>
+                    <span class="titlespan">Historia Evolucion</span>
                     <hr>
                     <div class="container inner rounded bg-white">
                         <br>
-                    <?php include_once('table.php') ?>
+                    <?php include_once('table_evoluciones.php') ?>
                     </div>
                 </div>
             </aside>
---
+++ Views/table_evoluciones.php
@@ -0,0 +1,28 @@
+
+<table class="table  table-hover bg-white">
+                        
+    <tbody>
+        
+        <tr>
+        <td><button type="submit" name="client" value="Alvarez, Jose" class="btn tabBtn btn-link">Alvarez, Jose</button></td>
+        </tr>
+        <tr>
+        <td><button type="submit" name="client" value="Bazan, Martin" class="btn tabBtn btn-link">Bazan, Martin</button></td>
+        </tr>
+        <tr>
+        <td><button type="submit" name="client" value="Conti, Victoria" class="btn tabBtn btn-link">Conti, Victoria</button></td>
+        </tr>
+        <tr>
+        <td><button type="submit" name="client" value="Corvalan, Martina" class="btn tabBtn btn-link">Corvalan, Martina</button></td>
+        </tr>
+        <tr>
+        <td><button type="submit" name="client" value="Costas, Sebastian" class="btn tabBtn btn-link">Costas, Sebastian</button></td>
+        </tr>
+        <tr>
+        <td><button type="submit" name="client" value="Crespo, Jesus" class="btn tabBtn btn-link">Crespo, Jesus</button></td>
+        </tr>
+        </tr>
+
+    </tbody>
+</table>
+</form>
