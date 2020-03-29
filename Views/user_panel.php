93cc8800c7ee43abf32ea164b5e6384f627d5cd1
Jlafratta <52461452+Jlafratta@users.noreply.github.com>
2020-03-29 06:56:49 -0300

    .

---
+++ Views/user_panel.php
@@ -0,0 +1,54 @@
+<html>
+    <head>
+        <title>AegirSystem</title>
+
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+        <link href="css/style.css" rel="stylesheet" type="text/css" />
+    </head>
+    <body class="bgdark">
+
+        <nav class="navbar  bg-dark navbar-dark ">
+
+            <span class="navbar-text">
+                <a href="https://www.aegirsystem.com/">Aegir System</a>
+            </span>
+
+            <span class="navbar-text">
+                Inicio
+            </span>
+
+            <button type="button" class="btn btn-outline-secondary">Salir</button>
+
+        </nav>
+        
+        <div class="container rounded my-3 ">
+
+        <aside>
+            <div class="container rounded p-3 my-3 bg-gray">
+                <h4>Pacientes</h4>
+                <div class="container inner rounded bg-white">
+
+                </div>
+            </div>
+        </aside>
+
+        <section>
+            <div class="container rounded p-3 my-3 bg-gray">
+                <form action="new_meeting.php" method="post">
+                    <button type="submit" class="btn btn-primary">Nueva reunión</button>
+                </form>
+            </div>
+        </section>
+        
+        </div>
+        
+
+    </body>
+</html>
